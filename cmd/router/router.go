157ffad5bb3c6d6455a400b6b5c15fb2e11b077d
Jeff Stock <jeffcstock@gmail.com>
2025-11-11 13:41:32 -0800

    add compatibity for slashses

--- cmd/router/router.go
+++ cmd/router/router.go
@@ -17,18 +17,27 @@ import (
 func SetupRouter() *httprouter.Router {
 	router := httprouter.New()
 
-	// V2 Routes
+	// V2 Routes (with and without trailing slash)
+	router.GET("/v2", GetCapacityAndNonCapacitySailings)
 	router.GET("/v2/", GetCapacityAndNonCapacitySailings)
+	router.GET("/v2/capacity", GetCapacitySailings)
 	router.GET("/v2/capacity/", GetCapacitySailings)
+	router.GET("/v2/capacity/:routeCode", GetSingleCapacityRoute)
 	router.GET("/v2/capacity/:routeCode/", GetSingleCapacityRoute)
+	router.GET("/v2/noncapacity", GetNonCapacitySailings)
 	router.GET("/v2/noncapacity/", GetNonCapacitySailings)
+	router.GET("/v2/noncapacity/:routeCode", GetSingleNonCapacityRoute)
 	router.GET("/v2/noncapacity/:routeCode/", GetSingleNonCapacityRoute)
 
-	// V1 Routes
+	// V1 Routes (with and without trailing slash)
+	router.GET("/api", GetAllSailings)
 	router.GET("/api/", GetAllSailings)
+	router.GET("/api/:departureTerminal", GetSailingsByDepartureTerminal)
 	router.GET("/api/:departureTerminal/", GetSailingsByDepartureTerminal)
+	router.GET("/api/:departureTerminal/:destinationTerminal", GetSailingsByDepartureAndDestinationTerminals)
 	router.GET("/api/:departureTerminal/:destinationTerminal/", GetSailingsByDepartureAndDestinationTerminals)
 
+	router.GET("/healthcheck", HealthCheck)
 	router.GET("/healthcheck/", HealthCheck)
 
 	router.NotFound = http.FileServer(http.Dir("./static"))
